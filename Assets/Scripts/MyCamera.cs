46474d2a64e9c9b9c330e366a3562c48ca55056d
Unknown <pasi.hanhijarvi@gmail.com>
2018-05-08 07:16:35 +0300

    Add placeholder character and camera follow

---
+++ Assets/Scripts/MyCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCamera : MonoBehaviour {
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = player.transform.position;
+
+        pos.y = transform.position.y;
+        pos.z -= 20;
+        transform.position = pos;
+	}
+}
